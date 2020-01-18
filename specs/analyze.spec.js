606c562b5a4d9cca8a2bf030cc585042b08a123b
Simon Repp <simon@fdpl.io>
2020-01-18 23:24:56 +0100

    Export sections at correct depths in schema generator

--- specs/analyze.spec.js
+++ specs/analyze.spec.js
@@ -3,10 +3,17 @@ const path = require('path');
 const analyze = require('../lib/analyze.js');
 
 const DOCUMENT_PATH = path.join(__dirname, 'fixtures/document.eno');
+const SECTIONS_DOCUMENT_PATH = path.join(__dirname, 'fixtures/sections.eno');
 
 describe('analyze', () => {
   it('builds an abstract syntax tree from a document', () => {
     const astDocument = analyze(DOCUMENT_PATH);
+
+    expect(astDocument).toMatchSnapshot();
+  });
+
+  it('builds an abstract syntax tree from a section-heavy document', () => {
+    const astDocument = analyze(SECTIONS_DOCUMENT_PATH);
 
     expect(astDocument).toMatchSnapshot();
   });
--- specs/generators/schema.spec.js
+++ specs/generators/schema.spec.js
@@ -4,10 +4,18 @@ const analyze = require('../../lib/analyze.js');
 const generateSchema = require('../../lib/generators/schema.js');
 
 const DOCUMENT_PATH = path.join(__dirname, '../fixtures/document.eno');
+const SECTIONS_DOCUMENT_PATH = path.join(__dirname, '../fixtures/sections.eno');
 
 describe('generateSchema', () => {
   it('generates a schema from an abstract syntax tree', () => {
     const astDocument = analyze(DOCUMENT_PATH);
+    const schema = generateSchema(astDocument);
+
+    expect(schema).toMatchSnapshot();
+  });
+
+  it('correctly exports section depths', () => {
+    const astDocument = analyze(SECTIONS_DOCUMENT_PATH);
     const schema = generateSchema(astDocument);
 
     expect(schema).toMatchSnapshot();
