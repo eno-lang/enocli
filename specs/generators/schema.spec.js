7e560d92a30b1be1f8bae871802b19c51672223e
Simon Repp <simon@fdpl.io>
2020-01-19 16:58:14 +0100

    Use inferred type information when generating the schema target

--- specs/generators/schema.spec.js
+++ specs/generators/schema.spec.js
@@ -5,6 +5,7 @@ const generateSchema = require('../../lib/generators/schema.js');
 
 const DOCUMENT_PATH = path.join(__dirname, '../fixtures/document.eno');
 const SECTIONS_DOCUMENT_PATH = path.join(__dirname, '../fixtures/sections.eno');
+const TYPES_DOCUMENT_PATH = path.join(__dirname, '../fixtures/types.eno');
 
 describe('generateSchema', () => {
   it('generates a schema from an abstract syntax tree', () => {
@@ -16,6 +17,13 @@ describe('generateSchema', () => {
 
   it('correctly exports section depths', () => {
     const astDocument = analyze(SECTIONS_DOCUMENT_PATH);
+    const schema = generateSchema(astDocument);
+
+    expect(schema).toMatchSnapshot();
+  });
+
+  it('correctly exports inferred types', () => {
+    const astDocument = analyze(TYPES_DOCUMENT_PATH);
     const schema = generateSchema(astDocument);
 
     expect(schema).toMatchSnapshot();
