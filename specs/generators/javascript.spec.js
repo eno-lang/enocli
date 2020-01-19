dcea5f95317f1d6f4a82720ab77da5a747b03570
Simon Repp <simon@fdpl.io>
2020-01-19 17:09:23 +0100

    Use inferred type information when generating javascript code

---
+++ specs/generators/javascript.spec.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const analyze = require('../../lib/analyze.js');
+const generateJavascript = require('../../lib/generators/javascript.js');
+
+const DOCUMENT_PATH = path.join(__dirname, '../fixtures/document.eno');
+const TYPES_DOCUMENT_PATH = path.join(__dirname, '../fixtures/types.eno');
+
+describe('generateJavascript', () => {
+  it('generates javascript code from an abstract syntax tree', () => {
+    const astDocument = analyze(DOCUMENT_PATH);
+    const javascript = generateJavascript(astDocument);
+
+    expect(javascript).toMatchSnapshot();
+  });
+
+  it('correctly exports inferred types', () => {
+    const astDocument = analyze(TYPES_DOCUMENT_PATH);
+    const javascript = generateJavascript(astDocument);
+
+    expect(javascript).toMatchSnapshot();
+  });
+});
